48e684c6cbe12cac7af10e017e4f4c12ccb5f002
MahfizaYasmin <mahfizayasmin@gmail.com>
2024-03-27 01:48:19 +0600

    added json file and create json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -6,8 +8,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     
     </>
   )
