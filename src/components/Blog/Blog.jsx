c37f62aca17e769b3afb789b6c84880db40b8407
MahfizaYasmin <mahfizayasmin@gmail.com>
2024-03-31 00:56:40 +0600

    added blog button and bookmarks

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { FaBookmark } from "react-icons/fa";
+const Blog = ({blog, handleAddToBookmark}) => {
+     
+    const {title, cover, author, posted_date, author_img, reading_time, hashtags} = blog;
+
+    return (
+        <div className='mb-20'>
+            <img className='w-full rounded-lg mb-8' src={cover} alt={`Cover picture of the title ${title}`}></img>
+            <div className='flex justify-between mb-4'>
+                <div className='flex gap-4'>
+                    <img className='w-14' src={author_img}></img>
+                    <div>
+                        <h2 className='text-2xl font-bold '>{author}</h2>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className='text-4xl mb-4'>{title}</h2>
+            <p className='flex gap-4'>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a>#{hash}</a></span>)
+                }
+            </p>
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func.isRequired
+}
+
+export default Blog;
